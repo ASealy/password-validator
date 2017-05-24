085ace663ba788f175c0de32c7ac5f646a1f591c
Andrew Sealy <andrewjsealy@live.ca>
2017-05-24 11:26:27 -0300

    a2-stage1

--- app/src/test/java/com/example/password_validator/ExampleUnitTest.java
+++ app/src/test/java/com/example/password_validator/ExampleUnitTest.java
@@ -14,4 +14,10 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void validate(){
+        assertTrue(!MainActivity.password.equals("password") && MainActivity.password.length()>=8);
+
+    }
 }
